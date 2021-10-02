fa4c5384e5609f69f9de274aadb480258fcba35d
victorandruscagit <victor.v.andrusca@gmail.com>
2021-10-02 16:56:07 +0300

    tomcat server , eclipse jetty server, code CityRescources

--- germes-service/src/main/java/org/itsimulator/germes/app/rest/service/CityResource.java
+++ germes-service/src/main/java/org/itsimulator/germes/app/rest/service/CityResource.java
@@ -3,8 +3,16 @@ package org.itsimulator.germes.app.rest.service;
 import java.util.List;
 import jersey.repackaged.com.google.common.collect.Lists;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+
+@Path("cities")
 public class CityResource {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public List<String> findCities(){
         return Lists.newArrayList("Odessa", "Kiev");
 

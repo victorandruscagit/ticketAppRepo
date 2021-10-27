3bd7ff5a1d52fce6915321f286710593ad4720df
victorandruscagit <victor.v.andrusca@gmail.com>
2021-10-27 16:15:18 +0300

    Pavel bilirubina 27.10, ora 16:16

--- germes-service/src/main/java/org/itsimulator/germes/app/rest/service/config/JerseyConfig.java
+++ germes-service/src/main/java/org/itsimulator/germes/app/rest/service/config/JerseyConfig.java
@@ -8,6 +8,7 @@ import javax.ws.rs.ApplicationPath;
 
 @ApplicationPath("api")
 public class JerseyConfig extends ResourceConfig {
+
     public JerseyConfig() {
         packages("org.itsimulator.germes.app.rest");
     }
--- germes-service/src/test/java/org/itsimulator/germes/app/rest/rest/CityResourceTest.java
+++ germes-service/src/test/java/org/itsimulator/germes/app/rest/rest/CityResourceTest.java
@@ -1,2 +1,25 @@
-package org.itsimulator.germes.app.rest.rest;public class CityResourceTest {
+package org.itsimulator.germes.app.rest.rest;
+
+import org.glassfish.jersey.test.JerseyTest;
+import org.itsimulator.germes.app.rest.service.config.JerseyConfig;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import javax.ws.rs.core.Application;
+import java.util.List;
+
+public class CityResourceTest extends JerseyTest {
+    @Override
+    protected Application configure() {
+        return new JerseyConfig();
+    }
+
+    @Test
+    public void testFindCities() {
+        List<?> cities = target("cities").request().get(List.class);
+        assertNotNull(cities);
+        assertTrue(cities.contains("Odessa"));
+        assertTrue(cities.contains("Kiev"));
+
+    }
 }
